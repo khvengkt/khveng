f11bdd83dc695fdfc5db90a2f3467a202ab75c37
User <User@DESKTOP-8A0IV92>
2024-03-24 10:19:38 +0700

    Add project files.

---
+++ khveng/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace khveng
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtpsw = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnout = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(3, -2);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(783, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ចូលប្រើប្រពន្ធ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ឈ្មោះអ្នកប្រើប្រាស់";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(241, 223);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(458, 32);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUsername_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ពាក្យសម្ងាត់";
+            // 
+            // txtpsw
+            // 
+            this.txtpsw.Location = new System.Drawing.Point(241, 281);
+            this.txtpsw.Name = "txtpsw";
+            this.txtpsw.PasswordChar = '*';
+            this.txtpsw.Size = new System.Drawing.Size(458, 32);
+            this.txtpsw.TabIndex = 1;
+            this.txtpsw.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtpsw_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::khveng.Properties.Resources.login__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(343, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Image = global::khveng.Properties.Resources.login_button;
+            this.btnlogin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnlogin.Location = new System.Drawing.Point(474, 335);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(100, 52);
+            this.btnlogin.TabIndex = 2;
+            this.btnlogin.Text = "ចូលប្រើ";
+            this.btnlogin.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // btnout
+            // 
+            this.btnout.Image = global::khveng.Properties.Resources.logout;
+            this.btnout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnout.Location = new System.Drawing.Point(598, 335);
+            this.btnout.Name = "btnout";
+            this.btnout.Size = new System.Drawing.Size(101, 52);
+            this.btnout.TabIndex = 3;
+            this.btnout.Text = "បោះបង់";
+            this.btnout.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnout.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnout);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtpsw);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Khmer OS Content", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtpsw;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btnout;
+    }
+}
+
---
+++ khveng/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace khveng
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "admin" && txtpsw.Text == "admin")
+            {
+                MessageBox.Show("ចូលប្រើប្រពន្ធជោគជ័យ");
+            }
+            else
+            {
+                MessageBox.Show("ចូលប្រើប្រពន្ធបរាជ័យ");
+            }
+        }
+
+        private void txtUsername_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                txtpsw.Focus();
+            }
+        }
+
+        private void txtpsw_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                btnlogin.PerformClick();
+            }
+        }
+    }
+}
